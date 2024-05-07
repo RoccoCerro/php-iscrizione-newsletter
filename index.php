664c23888add9515d64ff455666b2c2834588116
RoccoCerro <rcerro02@gmail.com>
2024-05-07 14:46:30 +0200

    Add: style with bootstrap

--- index.php
+++ index.php
@@ -17,22 +17,26 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-  <form action="./index.php" method="POST">
-    <input type="email" placeholder="Inserisci la tua email" name="email">
-    <input type="submit" value="Invia">
-  </form>
-  <div class="alert alert-dark" role="alert">
-    <?php
-    if(email_is_valid($email) === true){
-      ?>
-      Successo!
-      <?php
-    }else{
-      ?>
-      Fallito!
-      <?php
-    }
-    ?>
+  <div class="container p-5 bg-dark rounded mt-5">
+    <div class="row justify-content-center text-center ">
+      <form action="./index.php" method="POST" class="col-12">
+        <input type="email" placeholder="Inserisci la tua email" name="email" value="<?= $email ?>">
+        <input type="submit" value="Invia">
+      </form>
+      <div class="alert alert-dark w-50 mt-5 col-12" role="alert">
+        <?php
+        if(email_is_valid($email) === true){
+          ?>
+          Successo!
+          <?php
+        }else{
+          ?>
+          Fallito!
+          <?php
+        }
+        ?>
+      </div>
+    </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
